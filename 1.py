26c16fdd90ac158686d21a743b98ad062b1cc9dc
stand4r <sahojko.dima2018@gmail.com>
2022-12-18 16:51:06 +0400

    del files

--- 1.py
+++
@@ -1,8 +0,0 @@
-import pyrogram
-
-number = "+79171418765"
-api_id = 7640328
-api_hash = "e8fecf361251bc064b959f193f04a308"
-session = pyrogram.Client("23323", api_id, api_hash)
-session.start()
-print(session.get_me())
